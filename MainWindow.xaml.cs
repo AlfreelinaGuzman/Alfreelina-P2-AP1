ad1db32df01f670296e3a7076cb1763f27eb44a1
AlfreelinaGuzman <alfreelinaguzman@gmail.com>
2020-11-02 13:49:37 -0500

    creando las carpetas y diseño registro/ consulta

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Alfreelina_P2_AP1.UI.Registros;
+using Alfreelina_P2_AP1.UI.Consultas;
+
+namespace Alfreelina_P2_AP1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Registrar_Click(object sender, RoutedEventArgs e)
+        {
+            registro ventana = new registro();
+            ventana.Show();
+        }
+
+        private void Consultar_Click(object sender, RoutedEventArgs e)
+        {
+            consulta ventana = new consulta();
+            ventana.Show();
+        }
+    }
+}
---
+++ UI/Consultas/consulta.xaml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace  Alfreelina_P2_AP1.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class consulta : Window
+    {
+
+        public consulta ()
+        {
+            InitializeComponent();
+        }
+    }
+}
